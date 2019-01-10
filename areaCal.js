8edc383cca8fac6c081b0884c87eca89f4992c23
lora1221 <lori.tran1221@gmail.com>
2019-01-10 10:05:30 -0600

    first upload

---
+++ areaCal.js
@@ -0,0 +1,5 @@
+var width, height, area;
+width = parseInt(prompt("Enter the width: "));
+height = parseInt(prompt("Enter the height: "));
+area = width * height;
+document.write("Area is: " + area);
